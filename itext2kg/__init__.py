0171533cd5594201fbfa8cfa87af824a93a31c70
Yassir LAIRGI <yassirlairgi@gmail.com>
2025-10-28 09:59:30 +0100

    feat: expand module exports in __init__.py to include new components
    - Added Neo4jStorage, DocumentsDistiller, and LangchainOutputParser to the __all__ list for improved accessibility.
    - Enhanced module structure by integrating additional functionalities for better usability.

--- itext2kg/__init__.py
+++ itext2kg/__init__.py
@@ -1,3 +1,7 @@
 from itext2kg.atom import Atom
 from itext2kg.itext2kg_star import iText2KG, iText2KG_Star
-__all__ = ["Atom", "iText2KG", "iText2KG_Star"]
+from itext2kg.graph_integration import Neo4jStorage
+from itext2kg.documents_distiller import DocumentsDistiller
+from itext2kg.llm_output_parsing import LangchainOutputParser
+
+__all__ = ["Atom", "iText2KG", "iText2KG_Star", "Neo4jStorage", "DocumentsDistiller", "LangchainOutputParser"]
